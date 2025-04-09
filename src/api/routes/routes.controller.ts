8ee6f41e97a4f87d5f867579f405ffb86d7c6bdd
CarlosjctDev <carlosjulio0605@hotmail.com>
2025-04-08 22:45:44 -0500

    upload

--- src/api/routes/routes.controller.ts
+++ src/api/routes/routes.controller.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import { opendir } from 'node:fs/promises';
 import { join } from 'node:path';
 import { logger } from '#src/util/logger/logger.ts';
+import { pathToFileURL } from 'node:url';
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -11,33 +12,35 @@ export const loadRoutesDinamic = async (
     try {
         const dir = await opendir(routesDir);
         for await (const dirent of dir) {
-          if (dirent.isDirectory()) {
-            let name = dirent.name;                        
-            if (!/^[A-Z]/.test(name)) {
-                console.warn(`⛔ Carpeta ignorada (no inicia en MAYÚSCULA): "${name}"`);
-                continue;
-            }
-            name = name.toLocaleLowerCase();
-            const extension = isProd ? 'js' : 'ts';
-            const filename = `${name}.route.${extension}`;            
-            const fullPath = join(routesDir, name, filename);
-    
-            try {
-              const mod = await import(fullPath);              
-              const router = Object.values(mod).find((val) => val instanceof Hono);
-              const endPoint =  `${baseRoute}/${name}`;
-              if (!router) {
-                logger.warn(`⚠️ No se encontró una instancia de Hono en "${fullPath}"`);
-                continue;
-              }
-
-              app.route(endPoint, router);
-              // logger.info(`🌳 Ruta cargada: ${name} EndPoint: ${endPoint}`);
-            } catch (err: unknown) {
-                const message = err instanceof Error ? err.message : String(err);
-                logger.warn(`⚠️ No se pudo cargar la ruta para "${name}": ${message}`);
-            }
+          if (!dirent.isDirectory()) continue;          
+          let name = dirent.name;                        
+          if (!/^[A-Z]/.test(name)) {
+              console.warn(`⛔ Carpeta ignorada (no inicia en MAYÚSCULA): "${name}"`);
+              continue;
           }
+          name = name.toLocaleLowerCase();
+          const extension = isProd ? 'js' : 'ts';
+          const filename = `${name}.route.${extension}`;            
+          const fullPath = join(routesDir, name, filename);
+          const fileURL = pathToFileURL(fullPath).href;
+          logger.info(`🔍 Buscando archivo: ${fullPath}`);
+  
+          try {
+            const mod = await import(fileURL);              
+            const router = Object.values(mod).find((val) => val instanceof Hono);
+            const endPoint =  `${baseRoute}/${name}`;
+            if (!router) {
+              logger.warn(`⚠️ No se encontró una instancia de Hono en "${fullPath}"`);
+              continue;
+            }
+
+            app.route(endPoint, router);
+            // logger.info(`🌳 Ruta cargada: ${name} EndPoint: ${endPoint}`);
+          } catch (err: unknown) {
+              const message = err instanceof Error ? err.message : String(err);
+              logger.warn(`⚠️ No se pudo cargar la ruta para "${name}": ${message}`);
+          }
+          
         }
       } catch (err : unknown) {
         const message = err instanceof Error ? err.message : String(err);
